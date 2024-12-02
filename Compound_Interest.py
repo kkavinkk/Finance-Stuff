2e4f317940e3c3f59e8f38d47f4503d679cb2f73
kkavinkk <gk.kavin05@gmail.com>
2024-12-01 18:39:05 -0700

    added all functions and descripotion of functions

--- Compound_Interest.py
+++ Compound_Interest.py
@@ -1,2 +1,28 @@
-#UNTIL NEXT TIME
-#hello
+"""
+Easy compound interest calculator
+
+Compound Interest is shown by the function:
+
+A = P * (1 + r/n)^n*t
+
+A = Final amount
+r = Annual Interest Rate
+n = Number of times interest is compounded each year
+t = time in years
+"""
+import matplotlib as plt
+
+def get_user_values():
+#get all required values from user
+
+def calculate_compound_interest(principal, rate, frequency, time):
+#calculate the final amount as a function
+
+def plot_growth(principal, rate, frequency, time):
+#plot the growth of the investment
+
+def main():
+#allows us to run the calculator
+
+if __name__ == "__main__":
+    main()
